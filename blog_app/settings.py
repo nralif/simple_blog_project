9e1fdfe5e3acefaefc42ceca029c7c2c5568a2fd
nralif <naeemurrahman.aalif@gmail.com>
2021-06-02 16:57:00 +0600

    settings file update

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR /'static']
-STATIC_ROOT = BASE_DIR /'staticfiles'
-STATICFILES_STORAGE = 'whitnoise.storage.CompressedMainfestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
